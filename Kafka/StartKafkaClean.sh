7124d6423d521aada12307bd6a4a36d77bd9f772
Murthidn <murthi267@gmail.com>
2019-03-11 17:25:49 +0530

    Kafka Change

--- Kafka/StartKafkaClean.sh
+++ Kafka/StartKafkaClean.sh
@@ -6,32 +6,54 @@ container=$(ssh -o "StrictHostKeyChecking no" ubuntu@$node_name docker ps --form
 
 ENV_NAME=$(printf "$(hostname)")
 
-echo "Are you sure you want to clean Kafka Topics in \""$ENV_NAME"\" (y/n)?"
-read answer
+echo "Select type to clear Topic"
+echo "0: Clear Individual Topic"
+echo "1: Clear Multiple Topics"
+read clearType
 
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo ""
-    echo "Kafka Clean is under Progress in \""$ENV_NAME"\", please wait for 10 min..."
-    echo ""
+if [ "$clearType" -eq 1 ];then
 
-    start_time="`date "+%H:%M:%S"`";
-    echo "Strated at "$start_time
-    echo ""
+    echo "Are you sure you want to clean Kafka Topics in \""$ENV_NAME"\" (y/n)?"
+    read answer
 
-    sleep 10s
-    ssh ubuntu@$node_name docker exec -i $container sh kafka/KafkaClean.sh
+        if [ "$answer" != "${answer#[Yy]}" ] ;then
+            echo "Kafka Clean is under Progress in \""$ENV_NAME"\", please wait for 10 min..."
 
-    end_time="`date "+%H:%M:%S"`";
+            start_time="`date "+%H:%M:%S"`";
+            echo "Strated at "$start_time
 
-    echo ""    
-    echo "Kafka Clean is done!"
-    echo ""    
-    echo "Ended at "$end_time
-    echo ""
+            ssh ubuntu@$node_name docker exec -i $container sh kafka/KafkaClean.sh $clearType
 
-else
-    echo "Kafka Clean has been cancelled!"
-    echo ""
+            end_time="`date "+%H:%M:%S"`";
+
+            echo "Kafka Clean is done!"
+            echo "Ended at "$end_time
+
+            else
+            echo "Kafka Clean has been cancelled!"
+        fi
+
+else 
+    echo "Enter Topic Name to clear messages"
+    read topicName
+
+    echo "Are you sure you want to clean messages in Topics \""$topicName"\" (y/n)?"
+    read isClearIndvidualTopic
+
+        if [ "$isClearIndvidualTopic" != "${answer#[Yy]}" ] ;then
+            echo "Kafka Clean is under Progress in \""$ENV_NAME"\", please wait for 10 min..."
+
+            start_time="`date "+%H:%M:%S"`";
+            echo "Strated at "$start_time
+
+            ssh ubuntu@$node_name docker exec -i $container sh kafka/KafkaClean.sh $clearType $topicName
+
+            end_time="`date "+%H:%M:%S"`";
+
+            echo "Kafka Clean is done!"
+            echo "Ended at "$end_time
+
+            else
+            echo "Kafka Clean has been cancelled!"
+        fi
 fi
-
-#The End of file
