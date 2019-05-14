429e160843bbb96294d065d6c5812a13c4c31ac6
Murthidn <murthi267@gmail.com>
2019-05-14 16:54:10 +0530

    Kafka retention script

---
+++ Kafka/KafkaRetentionCheck.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#Get Kafka Node and Container:
+KAFKA_SVC_NAME=$(docker node ls --format "{{.Hostname}}" | grep "zk-kafka")
+node_name=($KAFKA_SVC_NAME)
+container=$(ssh -o "StrictHostKeyChecking no" ubuntu@$node_name docker ps --format "{{.Names}}" | grep kafka[0-9])
+
+
+#Get Running Kafka Topics:
+existingTopics=$(ssh ubuntu@$node_name docker exec -i $container /usr/share/kafka/bin/kafka-topics.sh --list --zookeeper zookeeper1:2181)
+array=($existingTopics)
+
+#Defining Colours
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "\nStarted Kafka Retention Value Verification...\n"
+
+#Loop and validate Topics:
+for (( i=0; i<${#array[@]}; i++ ))
+do   
+
+isRetentionExists=$(ssh ubuntu@$node_name docker exec -i $container /usr/share/kafka/bin/kafka-topics.sh --describe --zookeeper zookeeper1:2181 --topic "${array[$i]}" | grep Configs:retention.ms)
+    if [[ -z ${isRetentionExists} ]]; then #if null
+        echo -e ${GREEN}"INFO: Topic \"${array[$i]}\" Retention is default value (1 day)."${NC}
+
+    else
+        getRetentionValue=$(ssh ubuntu@$node_name docker exec -i $container /usr/share/kafka/bin/kafka-topics.sh --describe --zookeeper zookeeper1:2181 --topic "${array[$i]}" | awk '{ print $4}' | head -n 1 | sed 's/,.*//g' | cut -d '=' -f2)
+        echo -e ${YELLOW}"WARN: Topic \"${array[$i]}\" Retention value is \"$getRetentionValue\""${NC}
+    fi
+
+done
+echo -e "\nEnd: Verification is done.\n"
