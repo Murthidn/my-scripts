d1a190fe250610661f18356202134ae37ef762a3
Murthidn <murthi267@gmail.com>
2019-05-15 08:05:15 +0530

    Change script

--- Others/UpdateTenantConfig_Jenkins.sh
+++ Others/UpdateTenantConfig_Jenkins.sh
@@ -1,6 +1,28 @@
 #/bin/bash
 
+#Creating Jobs
+create_job(){
 
+echo -e "-----------------------------Start : $1 -----------------------------\n"
+
+#Checking the flag and creating if it doesn't exist
+isFlagExists=$(cat $2 | jq '.TenantInfo['$3'].analytics.'$1'.created')
+
+if [ $isFlagExists == "true" ]; then
+    echo -e "Flag already exists and enabled in $1\n"
+
+elif [ $isFlagExists == "false" ]; then
+    echo -e "Flag already exists and disabled in $1, going ahead and enabling the Flag\n"
+    jq '.TenantInfo['$tenants'].analytics.'$1' += {"created": true}' $2 > tmp.json && mv --force tmp.json $2                    
+
+else
+    echo -e "Falg doesn't exist in $1, going ahead and creating the flag\n"
+    jq '.TenantInfo['$tenants'].analytics.'$1' += {"created": true}' $2 > tmp.json && mv --force tmp.json $2
+fi
+
+echo -e "-----------------------------End : $jobType -----------------------------\n\n"
+
+}
 #Function to find and update Tenant
 find_update_tenant(){
 
@@ -28,70 +50,15 @@ for tenants in $(jq '.TenantInfo | keys | .[]' $1); do
           #Looping the jobs
           for jobType in $jobTypeArr; do
 
-              if [ $jobType == "PreparedData" ]; then
-                   
-                   echo -e "-----------------------------Start : $jobType -----------------------------\n"
-
-                   #Checking the flag and creating if it doesn't exist in PreparedData
-                   isCreateInPD=$(cat $1 | jq '.TenantInfo['$tenants'].analytics.PreparedData.created')
-
-                   if [ $isCreateInPD == "true" ]; then
-	                      echo -e "Flag already exists and enabled in PreparedData\n"
-
-                   elif [ $isCreateInPD == "false" ]; then
-	                        echo -e "Flag already exists and disabled in PreparedData, going ahead and enabling the Flag\n"
- 		                      jq '.TenantInfo['$tenants'].analytics.PreparedData += {"created": true}' $1 > tmp.json && mv --force tmp.json $1                    
-
-                   else
-	                        echo -e "Falg doesn't exist in PreparedData, going ahead and creating the flag\n"
- 		                      jq '.TenantInfo['$tenants'].analytics.PreparedData += {"created": true}' $1 > tmp.json && mv --force tmp.json $1
-                   fi
-
-                   echo -e "-----------------------------End : $jobType -----------------------------\n\n"
-
+              if [ $jobType == "PreparedData" ]; then                  
+              create_job $jobType $1 $tenants  
 
               elif [ $jobType == "MatchStore" ]; then
-
-                   echo -e "-----------------------------Start : $jobType -----------------------------\n"
-
-                     #Checking the flag and creating if it doesn't exist in PreparedData
-                     isCreateInMS=$(cat $1 | jq '.TenantInfo['$tenants'].analytics.MatchStore.created')
-
-                     if [ $isCreateInMS == "true" ]; then
-	                        echo -e "Flag already exists and enabled in MatchStore\n"
-
-                     elif [ $isCreateInMS == "false" ]; then
-	                          echo -e "Flag already exists and disabled in MatchStore, going ahead and enabling the Flag\n"
- 		                        jq '.TenantInfo['$tenants'].analytics.MatchStore += {"created": true}' $1 > tmp.json && mv --force tmp.json $1
-
-                     else
-	                          echo -e "Falg doesn't exist in MatchStore, going ahead and creating the flag\n"
- 		                        jq '.TenantInfo['$tenants'].analytics.MatchStore += {"created": true}' $1 > tmp.json && mv --force tmp.json $1
-                     fi
-
-                   echo -e "-----------------------------End : $jobType -----------------------------\n\n"
+              create_job $jobType $1 $tenants       
 
               elif [ $jobType == "EntityTransform" ]; then
-
-                   echo -e "-----------------------------Start : $jobType -----------------------------\n"
-
-                     #Checking the flag and creating if it doesn't exist in PreparedData
-                     isCreateInET=$(cat $1 | jq '.TenantInfo['$tenants'].analytics.EntityTransform.created')
-
-                     if [ $isCreateInET == "true" ]; then
-	                        echo -e "Flag already exists and enabled in EntityTransform\n"
-
-                     elif [ $isCreateInET == "false" ]; then
-	                          echo -e "Flag already exists and disabled in EntityTransform, going ahead and enabling the Flag\n"
- 		                        jq '.TenantInfo['$tenants'].analytics.EntityTransform += {"created": true}' $1 > tmp.json && mv --force tmp.json $1
-
-                     else
-	                          echo -e "Falg doesn't exist in EntityTransform, going ahead and creating the flag\n"
- 		                        jq '.TenantInfo['$tenants'].analytics.EntityTransform += {"created": true}' $1 > tmp.json && mv --force tmp.json $1
-                     fi
-
-                   echo -e "-----------------------------End : $jobType -----------------------------\n\n"
-
+              create_job $jobType $1 $tenants       
+               
             else
               echo -e "ERROR: $jobType job doesn't exist in storage account file, please verify.\n"
 
@@ -125,4 +92,4 @@ fi
 }
 
 #Main Program Starting
-update_tenant_config $1 $2 $3
+update_tenant_config $1 $2 $3 #inputFilePath.json tenantId jobName (PreparedData,EntityTransform,MatchStore)
